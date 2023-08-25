7a23431c065ab0b9f0e493274e53899fd47d3164
HemanthDas <103018057+HemanthDas@users.noreply.github.com>
2023-08-25 23:56:57 +0530

    completed the path and data should be changed along with additional changes

--- src/pages/journey.jsx
+++ src/pages/journey.jsx
@@ -1,7 +1,27 @@
+import jdata from "../assets/journeyData.json";
+import Set from "../components/set";
 const Journey = () => {
-  return <div className="journey">
-    <div className="sidebar"></div>
-  </div>;
+  const data = JSON.parse(JSON.stringify(jdata));
+
+  return (
+    <div className="journey">
+      <div className="up-spike">
+        {data.map(
+          (item) =>
+            item.id < 5 && (
+              <Set key={item.id} tag={item.id} txt={item.txt} type={"up"} title={item.year}/>
+            )
+        )}
+      </div>
+      <div className="full-line"></div>
+      <div className="down-spike">
+        {data.map(
+          (item) =>
+            item.id > 4 && <Set key={item.id} tag={item.id} txt={item.txt} title={item.year}/>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Journey;

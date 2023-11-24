5f9300ab9a5a3a47239178805642e886293e35f3
HemanthDas <hemanthchoudary4@gmail.com>
2023-11-24 11:14:28 +0530

    update App.css

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -4,18 +4,18 @@ const Table = (props) => {
   const { skillobject } = props;
   const keys = Object.keys(skillobject);
   return (
-    <div className="row">
+    <>
       {keys.map((element, index) => {
         return (
-          <div className="column" key={index}>
+          <>
             <h1>{element}</h1>
             {skillobject[element].map((skill, index) => {
               return <img src={skill} alt={skill} key={index} />;
             })}
-          </div>
+          </>
         );
       })}
-    </div>
+    </>
   );
 };
 Table.propTypes = {

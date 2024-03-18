a3d463336c98fbaf7c45167520337641fc61c0d2
HemanthDas <hemanthchoudary4@gmail.com>
2024-03-18 08:40:10 +0530

    Updage: Background changed

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <a className="item" href="#main">
+        Home
+      </a>
+      <a className="item" href="#personal">
+        About
+      </a>
+      <a className="item" href="#contact">
+        Projects
+      </a>
+    </nav>
+  );
+};
+export default Navbar;

bd1fc25c218ffcf5a3835212558b65ac4c22e70d
HemanthDas <hemanthchoudary4@gmail.com>
2024-04-26 13:35:09 +0530

    font change in page.css for aboutcard

--- src/components/aboutCard.jsx
+++ src/components/aboutCard.jsx
@@ -6,48 +6,18 @@ const AboutCard = ({ carddata }) => {
       <h2 className="card-t2">{carddata.main_title}</h2>
       <div className="expand">
         <div className="expand__content">
-          {carddata.details.map((detail) => {
+          {carddata.details.map((detail, index) => {
             return (
-              <>
+              <div key={index + 2} className="item">
                 <h3>{detail.title}</h3>
                 <ul>
                   {detail.points.map((point, index) => {
                     return <li key={index + 1}>{point}</li>;
                   })}
                 </ul>
-              </>
+              </div>
             );
           })}
-          <h3>Frontend Development:</h3>
-          <ul>
-            <li>
-              Proficient in HTML5, CSS3, and JavaScript for building interactive
-              and responsive user interfaces.
-            </li>
-            <li>
-              Experienced in leveraging frontend frameworks such as React.js for
-              building dynamic single-page applications (SPAs).
-            </li>
-            <li>
-              Skilled in using Bootstrap and Tailwind CSS for rapid prototyping
-              and creating visually appealing designs.
-            </li>
-          </ul>
-          <h3>Backend Development:</h3>
-          <ul>
-            <li>
-              Proficient in Node.js and Express.js for building scalable and
-              efficient server-side applications.
-            </li>
-            <li>
-              Experienced in working with MongoDB and Mongoose for developing
-              database-driven applications and handling data modeling.
-            </li>
-            <li>
-              Familiar with RESTful API design principles and implementing CRUD
-              operations for seamless client-server communication.
-            </li>
-          </ul>
         </div>
       </div>
     </div>
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,13 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import AboutCard from "../components/aboutCard";
 
 const About = () => {
   const about_ref = useRef(null);
-  const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            setIsVisible(true);
             entry.target.classList.add("about-appear");
           } else {
             entry.target.classList.remove("about-appear");
@@ -31,6 +29,64 @@ const About = () => {
             "Proficient in HTML5, CSS3, and JavaScript for building interactive and responsive user interfaces.",
             "Experienced in leveraging frontend frameworks such as React.js for building dynamic single-page applications (SPAs).",
             "Skilled in using Bootstrap and Tailwind CSS for rapid prototyping and creating visually appealing designs.",
+          ],
+        },
+        {
+          title: "Backend Development:",
+          points: [
+            "Proficient in Node.js and Express.js for building scalable and efficient server-side applications.",
+            "Experienced in working with MongoDB and Mongoose for developing database-driven applications and handling data modeling.",
+            "Familiar with RESTful API design principles and implementing CRUD operations for seamless client-server communication.",
+          ],
+        },
+      ],
+    },
+    {
+      small_title: "DevOps",
+      main_title: "DevOps",
+      details: [
+        {
+          title: "CI/CD Pipelines:",
+          points: [
+            "Experienced in setting up CI/CD pipelines using Jenkins and GitHub Actions for automating the build, test, and deployment processes.",
+            "Familiar with version control systems such as Git for managing codebase and collaborating with team members.",
+            "Skilled in using Docker for containerization and Kubernetes for orchestrating containerized applications.",
+          ],
+        },
+        {
+          title: "Cloud Platforms:",
+          points: [
+            "Proficient in working with cloud platforms such as AWS and Azure for deploying and scaling applications.",
+            "Experienced in setting up virtual machines, storage solutions, and networking configurations on cloud platforms.",
+            "Familiar with serverless computing and deploying functions on AWS Lambda for event-driven architectures.",
+          ],
+        },
+      ],
+    },
+    {
+      small_title: "GDSC",
+      main_title: "GOOGLE DEVELOPERS STUDENT CLUB",
+      details: [
+        {
+          title: "GDSC KLU",
+          points: [
+            "I am a member of the Google Developers Student Club (GDSC) at K L Deemed to be University, Vijayawada.",
+            "As a member, I actively participate in organizing workshops, and tech talks to promote collaboration and skill development among students.",
+            "I have also conducted sessions on web development, cloud computing, and competitive programming to enhance the technical knowledge of club members.",
+          ],
+        },
+      ],
+    },
+    {
+      small_title: "KL University",
+      main_title: "K L Deemed to be University",
+      details: [
+        {
+          title: "B.Tech in CSE",
+          points: [
+            "I am currently pursuing my Bachelor of Technology (B.Tech) in Computer Science and Engineering (CSE) at K L Deemed to be University, Vijayawada.",
+            "I have completed courses on data structures, algorithms, database management systems, and software engineering as part of my academic curriculum.",
+            "I have also participated in hackathons, coding competitions, and technical events to enhance my practical skills and knowledge.",
           ],
         },
       ],

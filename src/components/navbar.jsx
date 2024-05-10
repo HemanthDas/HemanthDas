c0465fc36afdd12b51cb9dc4a97cc08c95ecf83b
HemanthDas <hemanthchoudary4@gmail.com>
2024-05-10 10:09:09 +0530

    Respinsive navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,25 +8,52 @@ const Navbar = () => {
     };
   };
   return (
-    <nav className="animate">
-      <button className="item" onClick={handleClick("main")}>
-        Home
-      </button>
-      <button className="item" onClick={handleClick("project")}>
-        Projects
-      </button>
-      <button className="item" onClick={handleClick("achievements")}>
-        Achievements
-      </button>
-      <button
-        className="item"
+    <>
+      <nav className="animate">
+        <button className="item" onClick={handleClick("main")}>
+          Home
+        </button>
+        <button className="item" onClick={handleClick("project")}>
+          Projects
+        </button>
+        <button className="item" onClick={handleClick("achievements")}>
+          Achievements
+        </button>
+        <button
+          className="item"
+          onClick={() => {
+            document.getElementById("contact").style.display = "block";
+          }}
+        >
+          Contact
+        </button>
+      </nav>
+      <div
+        className="nav-toggle"
         onClick={() => {
-          document.getElementById("contact").style.display = "block";
+          document.querySelector(".nav-toggle").classList.toggle("active");
         }}
-      >
-        Contact
-      </button>
-    </nav>
+      ></div>
+      <nav className="responsive-nav">
+        <button className="item" onClick={handleClick("main")}>
+          Home
+        </button>
+        <button className="item" onClick={handleClick("project")}>
+          Projects
+        </button>
+        <button className="item" onClick={handleClick("achievements")}>
+          Achievements
+        </button>
+        <button
+          className="item"
+          onClick={() => {
+            document.getElementById("contact").style.display = "block";
+          }}
+        >
+          Contact
+        </button>
+      </nav>
+    </>
   );
 };
 export default Navbar;
